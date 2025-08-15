a18144749d62a9d1e5a8f7cc98022c6578798e32
Aditya Kumar Verma <80159890+skvinderr@users.noreply.github.com>
2025-08-15 14:20:27 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDL9SBAe9T5KXDUOc9zxHrKAfeAUEY2szE",
-  authDomain: "attendwise-tracker-app.firebaseapp.com",
-  projectId: "attendwise-tracker-app",
-  storageBucket: "attendwise-tracker-app.firebasestorage.app",
-  messagingSenderId: "1042613740448",
-  appId: "1:1042613740448:web:d569d29e0618f397a97ac5",
-  measurementId: "G-X2TN47K2YC"
-};
+
 
 
 // Initialize Firebase
@@ -23,4 +15,5 @@ const app = initializeApp(firebaseConfig);
 
 // Export the services you'll need in other parts of your app
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
